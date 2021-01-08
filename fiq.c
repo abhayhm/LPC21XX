e540feea688dd66ed36e9b7a1b7877d101b26b14
Abhay HM <abhayhmgi@gmail.com>
2021-01-08 18:42:08 +0530

    1/8/2021

--- fiq.c
+++ fiq.c
@@ -1,8 +1,8 @@
 /***********************************************************************/
-/*																																		 */
+/*													  				   */
 /*   Startup.s modified: line 263 ;FIQ_Handler     B     FIQ_Handler   */
-/*											 line 264 EXTERN FIQ_Handler									 */
-/*																																		 */
+/*	    				line 264 EXTERN FIQ_Handler					   */
+/*																	   */
 /***********************************************************************/
 
 #include <lpc21xx.h>
@@ -22,44 +22,43 @@ void delay_ms(unsigned int millisec)
 /*************************** FIQ handler(ISR) **************************/
 extern __irq void FIQ_Handler(void)
 {
-	unsigned int tc_state=T0TC; 																									/* Store current state of Timer0 */ 
+	unsigned int tc_state=T0TC; 																					/* Store current state of Timer0 */ 
 	
-	EXTINT |= EINT3_CONFIG;																												/* Clear EINT3 */
+	EXTINT |= EINT3_CONFIG;																							/* Clear EINT3 */
 	IOCLR0 = LED_PIN2;
-	delay_ms(2500);             																									/* Delay for 5 sec */
+	delay_ms(2500);             																					/* Delay for 5 sec */
 	IOSET0 = LED_PIN2;
 	
-	T0TC=tc_state;																																/*Restore previous state of Timer0 */
-	T0TCR=0x01;																																		/*Enable Timer0 */
-	
+	T0TC=tc_state;																									/*Restore previous state of Timer0 */
+	T0TCR=0x01;																																		
 	VICVectAddr = 0x00;
 }
 /*************************** IRQ handler(ISR) **************************/
 __irq void VIC_Int0_Handler(void)
 {
-	unsigned int tc_state=T0TC; 																									/* Store current state of Timer0 */
+	unsigned int tc_state=T0TC; 																					/* Store current state of Timer0 */
 	
-	EXTINT |= EINT2_CONFIG;																												/* Clear EINT2 */
+	EXTINT |= EINT2_CONFIG;																							/* Clear EINT2 */
 	IOCLR0 = LED_PIN1;
-	delay_ms(5000);																																/* Delay for 10 sec */
+	delay_ms(5000);																									/* Delay for 10 sec */
 	IOSET0 = LED_PIN1;
 	
-	T0TC=tc_state;																																/*Restore previous state of Timer0 */
-	T0TCR=0x01;																																		/*Enable Timer0 */
+	T0TC=tc_state;																									/*Restore previous state of Timer0 */
+	T0TCR=0x01;																										/*Enable Timer0 */
 	
 	VICVectAddr = 0x00;	
 }
 
 void Extint_Init()
 {
-	PINSEL0 |= PINSEL_EINT2|PINSEL_EINT3; 																				/*Configure P0.7 and P0.9 as external interrupt 2 and 3*/
-	EXTINT &= ~(EINT2_CONFIG|EINT3_CONFIG); 																			/*Clear external interrupt 2 and 3 in EXTINT register */
-	EXTMODE &= EINT2_CONFIG|EINT3_CONFIG; 																				/*Configure EINT2 and EINT3 as edge sensitive*/
-	EXTPOLAR &= EINT2_CONFIG|EINT3_CONFIG;																				/*Configure EINT2 and EINT3 as falling edge sensitive*/
+	PINSEL0 |= PINSEL_EINT2|PINSEL_EINT3; 																			/*Configure P0.7 and P0.9 as external interrupt 2 and 3*/
+	EXTINT &= ~(EINT2_CONFIG|EINT3_CONFIG); 																		/*Clear external interrupt 2 and 3 in EXTINT register */
+	EXTMODE &= EINT2_CONFIG|EINT3_CONFIG; 																			/*Configure EINT2 and EINT3 as edge sensitive*/
+	EXTPOLAR &= EINT2_CONFIG|EINT3_CONFIG;																			/*Configure EINT2 and EINT3 as falling edge sensitive*/
 
-	VICIntEnable |= EINT2|EINT3; 																									/* Enable EINT2(IRQ) PIN P0.3 and EINT3(FIQ)  P0.7 */
-	VICIntSelect |= EINT3;																												/*Configure EINT3 as FIQ*/
-	VICVectCntl0 = VICVECTCNT_ENABLE|VICSOFTINT_MAP;															/* Enable VICVectCntl0 and map EINT2 */
+	VICIntEnable |= EINT2|EINT3; 																					/* Enable EINT2(IRQ) PIN P0.3 and EINT3(FIQ)  P0.7 */
+	VICIntSelect |= EINT3;																							/*Configure EINT3 as FIQ*/
+	VICVectCntl0 = VICVECTCNT_ENABLE|VICSOFTINT_MAP;																/* Enable VICVectCntl0 and map EINT2 */
 	VICVectAddr0 = (unsigned) VIC_Int0_Handler; 																	/*Copy address or ISR EINT2*/
 }
 
@@ -68,8 +67,8 @@ void Extint_Init()
 int main(void)
 {	
 	Extint_Init();
-	IODIR0 = LED_PIN0|LED_PIN1|LED_PIN2;																					/* Set LED pins as output*/
-	IOSET0 = LED_PIN0|LED_PIN1|LED_PIN2;																					/* Off LED's */ 
+	IODIR0 = LED_PIN0|LED_PIN1|LED_PIN2;																			/* Set LED pins as output*/
+	IOSET0 = LED_PIN0|LED_PIN1|LED_PIN2;																			/* Off LED's */ 
 	
 	/********************* Blink LED0 each second ************************/
 	while(1)
