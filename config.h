bcd531d520199c56e744d1bb9d8714dd4f5c8773
Abhay HM <abhayhmgi@gmail.com>
2021-01-08 18:48:04 +0530

    1/8/2021

--- config.h
+++ config.h
@@ -4,18 +4,29 @@
 /*																	   */
 /***********************************************************************/
 
-#define LED_PIN0 (1<<0)															/* P0.0 as LED0 used in main()*/
-#define LED_PIN1 (1<<1)															/* P0.1 as LED1 used in __irq VIC_Int0_Handler()*/
-#define LED_PIN2 (1<<2)															/* P0.2 as LED2 used in FIQ FIQ_Handler()*/
+/* P0.0 as LED0 used in main()*/
+#define LED_PIN0 (1<<0)
+/* P0.1 as LED1 used in __irq VIC_Int0_Handler()*/
+#define LED_PIN1 (1<<1)									
+/* P0.2 as LED2 used in FIQ FIQ_Handler()*/
+#define LED_PIN2 (1<<2)															
 
-#define EINT2 (1<<16)															/* Select bit 16 (external interrupt 2) in interrupt table*/ 
-#define EINT3 (1<<17)															/* Select bit 17 (external interrupt 3) in interrupt table*/
+/* Select bit 16 (external interrupt 2) in interrupt table*/
+#define EINT2 (1<<16)															
+/* Select bit 17 (external interrupt 3) in interrupt table*/
+#define EINT3 (1<<17)															
 
-#define PINSEL_EINT2 (3<<14)													/* Selecting pin P0.7 */
-#define PINSEL_EINT3 (3<<18)													/* Selecting pin p0.9 */
+/* Selecting pin P0.7 */
+#define PINSEL_EINT2 (3<<14)													
+/* Selecting pin p0.9 */
+#define PINSEL_EINT3 (3<<18)													
 
-#define EINT2_CONFIG (1<<2)														/* Select external interrupt 2 in EXTINT */
-#define EINT3_CONFIG (1<<3)														/* Select external interrupt 3 in EXTINT */
+/* Select external interrupt 2 in EXTINT */
+#define EINT2_CONFIG (1<<2)														
+/* Select external interrupt 3 in EXTINT */
+#define EINT3_CONFIG (1<<3)														
 
-#define VICVECTCNT_ENABLE (1<<5)												/* Set enable pin for VICVectCntl */
-#define VICSOFTINT_MAP (1<<4)													/* Used for Mapping EINT2 0x10 */
+/* Set enable pin for VICVectCntl */
+#define VICVECTCNT_ENABLE (1<<5)												
+/* Used for Mapping EINT2 0x10 */
+#define VICSOFTINT_MAP (1<<4)													
--- fiq.c
+++ fiq.c
@@ -22,44 +22,63 @@ void delay_ms(unsigned int millisec)
 /*************************** FIQ handler(ISR) **************************/
 extern __irq void FIQ_Handler(void)
 {
-	unsigned int tc_state=T0TC; 																					/* Store current state of Timer0 */ 
+	/* Store current state of Timer0 */ 
+	unsigned int tc_state=T0TC; 																					
 	
-	EXTINT |= EINT3_CONFIG;																							/* Clear EINT3 */
+	/* Clear EINT3 */
+	EXTINT |= EINT3_CONFIG;																							
 	IOCLR0 = LED_PIN2;
-	delay_ms(2500);             																					/* Delay for 5 sec */
+	/* Delay for 5 sec */
+	delay_ms(2500);             																					
 	IOSET0 = LED_PIN2;
 	
-	T0TC=tc_state;																									/*Restore previous state of Timer0 */
-	T0TCR=0x01;																																		
+	/*Restore previous state of Timer0 */
+	T0TC=tc_state;		
+	/*Enable Timer0 */	
+	T0TCR=0x01;
+	
 	VICVectAddr = 0x00;
 }
 /*************************** IRQ handler(ISR) **************************/
 __irq void VIC_Int0_Handler(void)
 {
-	unsigned int tc_state=T0TC; 																					/* Store current state of Timer0 */
+	/* Store current state of Timer0 */
+	unsigned int tc_state=T0TC; 																					
 	
-	EXTINT |= EINT2_CONFIG;																							/* Clear EINT2 */
+	/* Clear EINT2 */
+	EXTINT |= EINT2_CONFIG;																							
 	IOCLR0 = LED_PIN1;
-	delay_ms(5000);																									/* Delay for 10 sec */
+	/* Delay for 10 sec */
+	delay_ms(5000);																									
 	IOSET0 = LED_PIN1;
 	
-	T0TC=tc_state;																									/*Restore previous state of Timer0 */
-	T0TCR=0x01;																										/*Enable Timer0 */
+	/*Restore previous state of Timer0 */
+	T0TC=tc_state;																									
+	/*Enable Timer0 */
+	T0TCR=0x01;																										
 	
 	VICVectAddr = 0x00;	
 }
 
 void Extint_Init()
 {
-	PINSEL0 |= PINSEL_EINT2|PINSEL_EINT3; 																			/*Configure P0.7 and P0.9 as external interrupt 2 and 3*/
-	EXTINT &= ~(EINT2_CONFIG|EINT3_CONFIG); 																		/*Clear external interrupt 2 and 3 in EXTINT register */
-	EXTMODE &= EINT2_CONFIG|EINT3_CONFIG; 																			/*Configure EINT2 and EINT3 as edge sensitive*/
-	EXTPOLAR &= EINT2_CONFIG|EINT3_CONFIG;																			/*Configure EINT2 and EINT3 as falling edge sensitive*/
+	/*Configure P0.7 and P0.9 as external interrupt 2 and 3*/
+	PINSEL0 |= PINSEL_EINT2|PINSEL_EINT3; 																			
+	/*Clear external interrupt 2 and 3 in EXTINT register */
+	EXTINT &= ~(EINT2_CONFIG|EINT3_CONFIG); 																		
+	/*Configure EINT2 and EINT3 as edge sensitive*/
+	EXTMODE &= EINT2_CONFIG|EINT3_CONFIG; 																			
+	/*Configure EINT2 and EINT3 as falling edge sensitive*/
+	EXTPOLAR &= EINT2_CONFIG|EINT3_CONFIG;																			
 
-	VICIntEnable |= EINT2|EINT3; 																					/* Enable EINT2(IRQ) PIN P0.3 and EINT3(FIQ)  P0.7 */
-	VICIntSelect |= EINT3;																							/*Configure EINT3 as FIQ*/
-	VICVectCntl0 = VICVECTCNT_ENABLE|VICSOFTINT_MAP;																/* Enable VICVectCntl0 and map EINT2 */
-	VICVectAddr0 = (unsigned) VIC_Int0_Handler; 																	/*Copy address or ISR EINT2*/
+	/* Enable EINT2(IRQ) PIN P0.3 and EINT3(FIQ)  P0.7 */
+	VICIntEnable |= EINT2|EINT3; 																					
+	/*Configure EINT3 as FIQ*/
+	VICIntSelect |= EINT3;																							
+	/* Enable VICVectCntl0 and map EINT2 */
+	VICVectCntl0 = VICVECTCNT_ENABLE|VICSOFTINT_MAP;																
+	/*Copy address or ISR EINT2*/
+	VICVectAddr0 = (unsigned) VIC_Int0_Handler; 																	
 }
 
 
@@ -67,8 +86,10 @@ void Extint_Init()
 int main(void)
 {	
 	Extint_Init();
-	IODIR0 = LED_PIN0|LED_PIN1|LED_PIN2;																			/* Set LED pins as output*/
-	IOSET0 = LED_PIN0|LED_PIN1|LED_PIN2;																			/* Off LED's */ 
+	/* Set LED pins as output*/
+	IODIR0 = LED_PIN0|LED_PIN1|LED_PIN2;																			
+	/* Off LED's */ 
+	IOSET0 = LED_PIN0|LED_PIN1|LED_PIN2;																			
 	
 	/********************* Blink LED0 each second ************************/
 	while(1)
